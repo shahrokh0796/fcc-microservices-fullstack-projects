a7bda0a198117a2a7e27b6b12aac18a31285d30e
Said <shahrokh07.96@gmail.com>
2024-05-07 13:56:28 +0530

    request-header-parser-microservice

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // index.js
 // where your node app starts
+const os = require('os');
 
 // init project
 require('dotenv').config();
@@ -9,6 +10,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
+const accepts = require('accepts');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -22,6 +24,24 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+// Get the netwrok interfaces
+const networkInterfaces = os.networkInterfaces();
+// Find the IPV4 address
+const address = Object.values(networkInterfaces)
+.flat().filter((interface) => interface.family  === "IPv4" && !interface.internal)
+.map((interface) => interface.address)[0];
+
+app.get('/api/whoami', (req, res) => {
+  // const accepts = accepts(req);
+  const language = req.headers['accept-language'] || 'en';
+  const software = req.headers['user-agent'];
+  res.json({
+    ipaddress: address, 
+    language: language,
+    software: software,
+  });
 });
 
 // listen for requests :)
