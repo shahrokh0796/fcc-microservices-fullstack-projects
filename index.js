7c4bc8c9ffaf9f694bf800916a8e9876482e3fdd
Said <shahrokh07.96@gmail.com>
2024-05-08 14:33:52 +0530

    url-shortener-microservice complete

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const shortid = require('shortid');
 const cors = require('cors');
 const app = express();
 
@@ -7,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -18,6 +20,28 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+const urlDatabase = {};
+app.post('/api/shorturl', (req, res) => {
+  let originalUrl = req.body.url;
+    if(!originalUrl) {
+      return res.status(400).json({error: "Missing URL in request body"})
+    }
+
+    const shortUrl = `http://localhost:${port}/${shortid.generate()}`;
+    urlDatabase[shortUrl] = originalUrl;
+    res.json({originalUrl, shortUrl});
+  });
+
+  app.get('/:shortUrl', (req, res) => {
+    const { shortUrl } = req.params;
+    const originalUrl = urlDatabase[`http://localhost${port}/${shortUrl}`];
+    if(!originalUrl) {
+      return res.status(404).json({error: 'Short URL not found'});
+    }
+
+    res.redirect(originalUrl);
+  });
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
